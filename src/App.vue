a1d0a454c14b598770e6324aea0946a0d7ae8f2b
Alexandru ISTRATII <aistratii@pentalog.com>
2019-05-03 14:40:19 +0300

    Set up Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
   <div id="app">
+    <navbar/>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+  import Navbar from "@/components/Navbar.vue";
 
-export default {
-  name: 'app',
-  components: {
+  export default {
+    name: 'app',
+    components: {
+      Navbar
+    }
   }
-}
 </script>
 
 <style>
+  * {
+    font-family: 'Poppins';
+    border-radius: 0px !important;
+  }
+
+  body {
+    background-color: #f5f3ef; 
+  }
 
 </style>
