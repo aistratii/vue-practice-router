7aae41d203c108af624e072d8da890999cee2e1e
Alexandru ISTRATII <aistratii@pentalog.com>
2019-05-07 15:48:50 +0300

    Spinner for loading a form request

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -29,7 +29,10 @@
                         </div>
                     </div>
                     <div class="form-group text-center">
-                        <button @click="registerUser()" class="btn btn-success form-control">Signup</button>
+                        <button @click="registerUser()" v-bind:disabled="loading" class="btn btn-success form-control">
+                            <i class="fas fa-spin fa-spinner" v-if="loading"></i>
+                            {{ loading ? '' : 'Signup'}} 
+                        </button>
                     </div>
                 </div>
             </div>
@@ -47,11 +50,14 @@ export default {
             email: "",
             password: "",
             errors: {},
-            submitted: false
+            submitted: false,
+            loading: false
         }
     },
     methods: {
         registerUser(){
+            this.loading = true;
+
             Axios.post(
                 'https://react-blog-api.bahdcasts.com/api/auth/register', 
                 {
@@ -60,6 +66,7 @@ export default {
                     password: this.password
                 }
             ).then(response => {
+                this.loading = false;
                 this.submitted = true;
                 const authUserData = response.data.data;
                 localStorage.setItem('auth', JSON.stringify(authUserData));
@@ -68,6 +75,8 @@ export default {
                 this.$router.push('home');
             }).catch(({response}) => {
                 console.error(response.data);
+                
+                this.loading = false;
                 this.submitted = true;
                 this.errors = response.data;
             });
