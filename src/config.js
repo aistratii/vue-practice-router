048fa63f0554979c5de94a2aa41ce5bffebdec22
Alexandru ISTRATII <aistratii@pentalog.com>
2019-05-08 16:17:43 +0300

    Added upload image functionality

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export default {
-    apiUrl: "https://react-blog-api.bahdcasts.com/api"
+    apiUrl: "https://react-blog-api.bahdcasts.com/api",
+    imageUploadUrl: "https://api.cloudinary.com/v1_1/dq9jkgrzx/image/upload"
 }
--- src/views/CreateArticle.vue
+++ src/views/CreateArticle.vue
@@ -4,10 +4,16 @@
             <div class="col-md-8 offset-md-2">
                 <div class="card my-5">
                     <div class="card-body">
+                        <picture-input 
+                            accept="image/jpeg,image/png" 
+                            size="10" 
+                            button-class="btn btn-danger"
+                            @change="onChange">
+                        </picture-input>
                         <input type="text" placeholder="Title" class="form-control mb-3">
                         <wysiwyg v-model="content"/>
                         <div class="text-center">
-                            <button class="btn btn-success mt-3">Create article</button>
+                            <button @click="createArticle" class="btn btn-success mt-3">Create article</button>
                         </div>
                     </div>
                 </div>
@@ -17,13 +23,39 @@
 </template>
 
 <script>
+import PictureInput from 'vue-picture-input';
+import Axios from 'axios';
+import config from "@/config";
+
 export default {
+    components: {
+        PictureInput
+    },
     data() {
         return {
-            content: ""
+            content: "",
+            image: null
+        }
+    },
+    methods: {
+        onChange(image){
+            this.image = image
+        },
+
+        createArticle(){
+            const form = new FormData();
+            form.append('file', this.image);
+            form.append('upload_preset', 'my-preset-name');
+            form.append('api_key', '432956187613698');
+
+            Axios.post(`${config.imageUploadUrl}`, form)
+            .then(response => {
+                console.log(response);
+            }).catch(error => {
+                console.error(error);
+            });
         }
     }
-    
 }
 </script>
 
